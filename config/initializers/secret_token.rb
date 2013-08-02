c3b25bcb9adca5d372c69dc2a34bdb68c0e7b3f9
sunil1293 <bg.sunilkumar1@gmail.com>
2013-08-02 12:24:05 +0530

    single signup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TimeSheetEffone::Application.config.secret_key_base = 'fa4241203e3efcdf006820d86adef4c8223946c2d2b2a3c1808d8babc8c39fbcd65e6ac09022654140d939e9512cc1d38f5d174a68a6da3055581fe8607e043e'
